a248d012f8ba1e8b923d9d3594a77858cc1c7b46
testing270505-rgb <testing270505@gmail.com>
2026-04-27 12:27:17 +0530

    Telegram Bot support

--- parameters.py
+++ parameters.py
@@ -6,6 +6,11 @@ env = environ.Env()
 if os.path.exists('.env'):
     environ.Env.read_env('.env')
 
+# Telegram Bot configuration
+TELEGRAM_ENABLED = env.bool("STRMNTR_TELEGRAM_ENABLED", False)
+TELEGRAM_BOT_TOKEN = env.str("STRMNTR_TELEGRAM_BOT_TOKEN", "")
+TELEGRAM_CHAT_ID = env.int("STRMNTR_TELEGRAM_CHAT_ID", 0)
+TELEGRAM_STATUS_INTERVAL = env.int("STRMNTR_TELEGRAM_STATUS_INTERVAL", 1800)  # 30 minutes
 
 DOWNLOADS_DIR = env.str("STRMNTR_DOWNLOAD_DIR", "downloads")
 MIN_FREE_DISK_PERCENT = env.float("STRMNTR_MIN_FREE_SPACE", 5.0)  # in %
@@ -20,7 +25,7 @@ FFMPEG_PATH = env.str("STRMNTR_FFMPEG_PATH", 'ffmpeg')
 # You can enter a number to select a specific height.
 # Use a huge number here and closest match to get the highest resolution variant
 # Eg: 240, 360, 480, 720, 1080, 1440, 99999
-WANTED_RESOLUTION = env.int("STRMNTR_RESOLUTION", 1080)
+WANTED_RESOLUTION = env.int("STRMNTR_RESOLUTION", 480)
 
 # Specify match type when specified height
 # Possible values: exact, exact_or_least_higher, exact_or_highest_lower, closest
@@ -53,7 +58,7 @@ FFMPEG_READRATE = env.int("STRMNTR_FFMPEG_READRATE", 1.3)
 # Example:
 # 1 hour
 # SEGMENT_TIME = '1:00:00'
-SEGMENT_TIME = env.str("STRMNTR_SEGMENT_TIME", None)
+SEGMENT_TIME = env.str("STRMNTR_SEGMENT_TIME", 3600)
 
 # HTTP Manager configuration
 
@@ -86,4 +91,4 @@ WEB_CONFIRM_DELETES = env.str("STRMNTR_CONFIRM_DEL", "MOBILE")
 
 # Password for the web server
 # If empty no auth required, else username admin and choosen password
-WEBSERVER_PASSWORD = env.str("STRMNTR_PASSWORD", "admin")
+WEBSERVER_PASSWORD = env.str("STRMNTR_PASSWORD", "manav")
